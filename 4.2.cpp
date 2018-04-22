b6b0254a79f0cc54bfcd5d850307ef073eeb0bdf
Alexey Onufriev <aonufriev@deloitte.com>
2018-04-23 00:44:02 +0300

    add laba

--- 4.2.cpp
+++ 4.2.cpp
@@ -1,54 +1,53 @@
-#include <conio>
+#include <fstream>
 #include <iostream>
-#include <iomanip>
-
+#include <string>
+#include <locale>
 
 using namespace std;
-template <typename T>
-T filter(T *mass, int size, bool flag) {
-    while(!flag) {
-        flag = true;
 
-        for(int i = 0; i < size-1; i++)
-            if(mass[i] < mass[i+1])
-            {
-                swap(mass[i],mass[i + 1]);
-
-                flag = false;
-            }
+template <class Type>
+class Vectora {
+  private:
+    Type v1x, v1y, v2x, v2y, re[4];
+  public:
+    Type sum (Type v1x, Type v1y, Type v2x, Type v2y) {
+      this->v1x = v1x;
+      this->v1y = v1y;
+      this->v2x = v2x;
+      this->v2x = v2x;
+      re[0] = v1x + v2x;
+      re[1] = v1y + v2y;
+      cout << "vector summ = {" << re[0] << ", " << re[1] << "}" << endl;
     }
-}
-
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-
-    const int size = 4;
-
-    bool flag = false;
-
-    double mass[size];
-
-    cout<<"\nВведите "<< size <<" элементов массива:\n";
-
-    for(int i = 0; i < size; i++)
-    {
-        cout<<"A [ "<< i <<" ]= ";
-        cin>>mass[i];
+    Type scalar () {
+      re[2] = this->v1x * this->v2x + this->v1y * this->v2y;
+      cout << "vector scalar = {" << re[2] << "}" << endl;
     }
+    Type addPi () {
+      const float pi = 3.14;
+      re[3] = pi * this->v1x;
+      re[4] = pi * this->v1y;
+      cout << "add const to vector = {" << re[3] << ", " << re[4] << "}" << endl;
+    }
+};
 
-    cout<<"\n\nВведенный массив:\n\n";
 
-    for(int i = 0; i < size; i++)
-        cout<<setw(4)<<mass[i];
+int main () {
+  Vectora<double> obj;
+  double v1x, v1y, v2x, v2y;
+  cout << "v1x => ";
+  cin >> v1x;
 
-    filter(mass, size, flag);
+  cout << "v1y => ";
+  cin >> v1y;
 
-    cout<<"\n\nОтсортированный массив:\n\n";
+  cout << "v2x => ";
+  cin >> v2x;
 
-    for(int i = 0; i < size; i++)
-        cout<<setw(4)<<mass[i]<<" ";
+  cout << "v2y => ";
+  cin >> v2y;
 
-    _getch();
-    return 0;
+  obj.sum(v1x, v1y, v2x, v2y);
+  obj.scalar();
+  obj.addPi();
 }
