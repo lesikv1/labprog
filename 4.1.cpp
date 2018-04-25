1e7a469ce7c94f441ef1abdb0ab29634b719be54
Alexey Onufriev <aonufriev@deloitte.com>
2018-04-25 17:13:34 +0300

    update 4 laba

--- 4.1.cpp
+++ 4.1.cpp
@@ -13,8 +13,53 @@ using namespace std;
 
 class Vectora {
   private:
-    int v1x, v1y, v2x, v2y, re[4];
+    int v1x, v1y, v2x, v2y, re[6];
   public:
+
+    void inputV () {
+      try {
+      int vv1x, vv1y, vv2x, vv2y;
+        vv1x = 0;
+        vv1y = 0;
+        vv2x = 0;
+        vv2y = 0;
+        bool i;
+        cout << "v1x => ";
+        cin >> vv1x;
+        if (!cin) {
+          cin >> vv1x;
+          cout << "v1x error" << endl;
+          throw 1;
+        }
+
+        cout << "v1y => ";
+        cin >> vv1y;
+        if (!cin) {
+          cin >> vv1y;
+          cout << "v1y error" << endl;
+          throw 1;
+        }
+
+        cout << "v2x => ";
+        cin >> vv2x;
+        if (!cin) {
+          cin >> vv2x;
+          cout << "v2x error" << endl;
+          throw 1;
+        }
+
+        cout << "v2y => ";
+        cin >> vv2y;
+        if (!cin) {
+          cin >> vv2y;
+          cout << "v2y error" << endl;
+          throw 1;
+        }
+        sum(vv1x, vv1y, vv2x, vv2y);
+      } catch (...) {
+        sum(3, 4, 6, 5);
+      }
+    }
     void sum (int v1x, int v1y, int  v2x, int v2y) {
       this->v1x = v1x;
       this->v1y = v1y;
@@ -23,35 +68,25 @@ class Vectora {
       re[0] = v1x + v2x;
       re[1] = v1y + v2y;
       cout << "vector summ = {" << re[0] << ", " << re[1] << "}" << endl;
+      scalar();
     }
   void scalar () {
     re[2] = this->v1x * this->v2x + this->v1y * this->v2y;
     cout << "vector scalar = {" << re[2] << "}" << endl;
+    addPi();
   }
   void addPi () {
     const float pi = 3.14;
     re[3] = pi * this->v1x;
     re[4] = pi * this->v1y;
     cout << "add const to vector = {" << re[3] << ", " << re[4] << "}" << endl;
+    re[5] = this->v1x * pi;
+    re[6] = this->v1y * pi;
+    cout << "add vector to const = {" << re[5] << ", " << re[6] << "}" << endl;
   }
 };
 
 int main () {
   Vectora start;
-  int v1x, v1y, v2x, v2y;
-  cout << "v1x => ";
-  cin >> v1x;
-
-  cout << "v1y => ";
-  cin >> v1y;
-
-  cout << "v2x => ";
-  cin >> v2x;
-
-  cout << "v2y => ";
-  cin >> v2y;
-
-  start.sum(v1x, v1y, v2x, v2y);
-  start.scalar();
-  start.addPi();
+  start.inputV();
 }
--- 4.2.cpp
+++ 4.2.cpp
@@ -15,8 +15,47 @@ using namespace std;
 template <class Type>
 class Vectora {
   private:
-    Type v1x, v1y, v2x, v2y, re[4];
+    Type v1x, v1y, v2x, v2y, re[6];
   public:
+    Type inputV () {
+      try {
+      double vv1x, vv1y, vv2x, vv2y;
+        cout << "v1x => ";
+        cin >> vv1x;
+        if (!cin) {
+          cin >> vv1x;
+          cout << "v1x error" << endl;
+          throw 1;
+        }
+
+        cout << "v1y => ";
+        cin >> vv1y;
+        if (!cin) {
+          cin >> vv1y;
+          cout << "v1y error" << endl;
+          throw 1;
+        }
+
+        cout << "v2x => ";
+        cin >> vv2x;
+        if (!cin) {
+          cin >> vv2x;
+          cout << "v2x error" << endl;
+          throw 1;
+        }
+
+        cout << "v2y => ";
+        cin >> vv2y;
+        if (!cin) {
+          cin >> vv2y;
+          cout << "v2y error" << endl;
+          throw 1;
+        }
+        sum(vv1x, vv1y, vv2x, vv2y);
+      } catch (...) {
+        sum(3, 4, 6, 5);
+      }
+    }
     Type sum (Type v1x, Type v1y, Type v2x, Type v2y) {
       this->v1x = v1x;
       this->v1y = v1y;
@@ -25,36 +64,27 @@ class Vectora {
       re[0] = v1x + v2x;
       re[1] = v1y + v2y;
       cout << "vector summ = {" << re[0] << ", " << re[1] << "}" << endl;
+      scalar();
     }
     Type scalar () {
       re[2] = this->v1x * this->v2x + this->v1y * this->v2y;
       cout << "vector scalar = {" << re[2] << "}" << endl;
+      addPi();
     }
     Type addPi () {
       const float pi = 3.14;
       re[3] = pi * this->v1x;
       re[4] = pi * this->v1y;
       cout << "add const to vector = {" << re[3] << ", " << re[4] << "}" << endl;
+      re[5] = this->v1x * pi;
+      re[6] = this->v1y * pi;
+      cout << "add vector to const = {" << re[5] << ", " << re[6] << "}" << endl;
     }
 };
 
 
 int main () {
   Vectora<double> obj;
-  double v1x, v1y, v2x, v2y;
-  cout << "v1x => ";
-  cin >> v1x;
 
-  cout << "v1y => ";
-  cin >> v1y;
-
-  cout << "v2x => ";
-  cin >> v2x;
-
-  cout << "v2y => ";
-  cin >> v2y;
-
-  obj.sum(v1x, v1y, v2x, v2y);
-  obj.scalar();
-  obj.addPi();
+  obj.inputV();
 }
